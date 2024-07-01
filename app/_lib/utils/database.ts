3cc4b81f354c21b7928ea19445df7d86644d61cf
Talha <talhasifat23@gmail.com>
2024-07-01 23:21:32 +0500

    init and database connection

---
+++ app/_lib/utils/database.ts
@@ -0,0 +1,22 @@
+import mongoose from "mongoose";
+import { env } from "process";
+const CONNECTION_STRING = process.env.MONGODB_CONNECTION_STRING;
+const connectToDB = async () => {
+  if (!CONNECTION_STRING) return;
+
+  mongoose.set("strictQuery", true);
+  if (mongoose.connection.readyState === mongoose.ConnectionStates.connected) {
+    console.log("already connected!");
+    return;
+  }
+  try {
+    const connection = await mongoose.connect(CONNECTION_STRING, {
+      dbName: "inventaryMangmentSys",
+    });
+    console.log("connected");
+    return connection;
+  } catch (err) {
+    console.log(err);
+  }
+};
+export default connectToDB;
